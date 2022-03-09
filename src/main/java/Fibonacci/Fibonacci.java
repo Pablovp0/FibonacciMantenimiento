f0dff85ae12aa20c2b6bf645b6600497193cd85c
Pablo <pvpcmg4@uma.es>
2022-03-09 20:59:41 +0100

    Método compute implementado.

--- src/main/java/Fibonacci/Fibonacci.java
+++ src/main/java/Fibonacci/Fibonacci.java
@@ -1,4 +1,22 @@
 package Fibonacci;
 
 public class Fibonacci {
+
+    public static int compute(int n){
+        if(n<=1){
+            return n;
+        }else{
+            int i = compute(n - 1) + compute(n - 2);
+            return i;
+         }
+    }
+
+
+
+    public static void main (String args[])
+    {
+
+        int n = 9;
+        System.out.println(compute(n));
+    }
 }
--- src/test/java/FibonacciTest/FibonacciTest.java
+++ src/test/java/FibonacciTest/FibonacciTest.java
@@ -1,4 +1,27 @@
 package FibonacciTest;
 
+import Fibonacci.Fibonacci;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FibonacciTest {
+
+    private Fibonacci fibonacci;
+
+    @BeforeEach
+    public void setUp(){
+        fibonacci = new Fibonacci();
+    }
+
+    @Test
+    public void testNumber0() {
+        int expectedResult = 0;
+
+        assertEquals(expectedResult, fibonacci.compute(0));
+    }
+
 }
